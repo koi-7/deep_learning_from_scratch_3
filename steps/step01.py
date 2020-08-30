54a97bbbf30840190c94accb4a3fdb3d1c712cd3
koi <kkoiwa.1720+git@gmail.com>
2020-08-30 14:35:09 +0900

    コード追加

--- steps/step01.py
+++ steps/step01.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
 # coding: utf-8
 
+import numpy as np
+
 class Variable:
     def __init__(self, data):
         self.data = data
+
+data = np.array(1.0)
+x = Variable(data)
+print(x.data)
+
+x.data = np.array(2.0)
+print(x.data)
