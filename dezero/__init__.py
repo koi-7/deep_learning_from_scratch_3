e7a4d1ca93011dedab4ce74da616ad17b33a656c
koi <kkoiwa.1720+git@gmail.com>
2020-09-02 17:04:06 +0900

    for step40

--- dezero/__init__.py
+++ dezero/__init__.py
@@ -21,4 +21,6 @@ else:
     from dezero.core import as_variable
     from dezero.core import setup_variable
 
+    import dezero.functions
+
 setup_variable()
--- dezero/core.py
+++ dezero/core.py
@@ -87,7 +87,6 @@ class Variable:
 
         while funcs:
             f = funcs.pop()
-            # gys = [output.grad for output in f.outputs]
             gys = [output().grad for output in f.outputs]
 
             with using_config('enable_backprop', create_graph):
@@ -168,11 +167,16 @@ class Function:
 
 class Add(Function):
     def forward(self, x0, x1):
+        self.x0_shape, self.x1_shape = x0.shape, x1.shape
         y = x0 + x1
         return y
 
     def backward(self, gy):
-        return gy, gy
+        gx0, gx1 = gy, gy
+        if self.x0_shape != self.x1_shape:
+            gx0 = dezero.functions.sum_to(gx0, self.x0_shape)
+            gx1 = dezero.functions.sum_to(gx1, self.x1_shape)
+        return gx0, gx1
 
 
 def add(x0, x1):
@@ -186,9 +190,13 @@ class Mul(Function):
         return y
 
     def backward(self, gy):
-        # x0, x1 = self.inputs[0].data, self.inputs[1].data
         x0, x1 = self.inputs
-        return gy * x1, gy * x0
+        gx0 = gy * x1
+        gx1 = gy * x0
+        if x0.shape != x1.shape:  # for broadcast
+            gx0 = dezero.functions.sum_to(gx0, x0.shape)
+            gx1 = dezero.functions.sum_to(gx1, x1.shape)
+        return gx0, gx1
 
 
 def mul(x0, x1):
@@ -211,11 +219,17 @@ def neg(x):
 
 class Sub(Function):
     def forward(self, x0, x1):
+        self.x0_shape, self.x1_shape = x0.shape, x1.shape
         y = x0 - x1
         return y
 
     def backward(self, gy):
-        return gy, -gy
+        gx0 = gy
+        gx1 = -gy
+        if self.x0_shape != self.x1_shape:
+            gx0 = dezero.functions.sum_to(gx0, self.x0_shape)
+            gx1 = dezero.functions.sum_to(gx1, self.x1_shape)
+        return gx0, gx1
 
 
 def sub(x0, x1):
@@ -234,10 +248,12 @@ class Div(Function):
         return y
 
     def backward(self, gy):
-        # x0, x1 = self.inputs[0].data, self.inputs[1].data
         x0, x1 = self.inputs
         gx0 = gy / x1
         gx1 = gy * (-x0 / x1 ** 2)
+        if x0.shape != x1.shape:  # for broadcast
+            gx0 = dezero.functions.sum_to(gx0, x0.shape)
+            gx1 = dezero.functions.sum_to(gx1, x1.shape)
         return gx0, gx1
 
 
