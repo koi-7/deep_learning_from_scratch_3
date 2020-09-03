deeade69ad53093ce70162f5978c63dfee52b596
koi <kkoiwa.1720+git@gmail.com>
2020-09-03 14:09:57 +0900

    for step44

--- dezero/__init__.py
+++ dezero/__init__.py
@@ -21,6 +21,8 @@ else:
     from dezero.core import as_variable
     from dezero.core import setup_variable
 
+    from dezero.core import Parameter
+
     import dezero.functions
 
 setup_variable()
