7665644bcf3d7658e0ac16f8e0e42449e0171fca
koi <kkoiwa.1720+git@gmail.com>
2020-09-03 18:34:27 +0900

    first commit

---
+++ steps/step47.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+# coding: utf-8
+
+
+if '__file__' in globals():
+    import os, sys
+    sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+
+import numpy as np
+from dezero import Variable, as_variable
+import dezero.functions as F
+from dezero.models import MLP
+
+
+model = MLP((10, 3))
+
+x = np.array([[0.2, -0.4]])
+y = model(x)
+print(y)
+print()
+
+
+def softmax1d(x):
+    x = as_variable(x)
+    y = F.exp(x)
+    sum_y = F.sum(y)
+    return y / sum_y
+
+
+x = Variable(np.array([[0.2, -0.4]]))
+y = model(x)
+p = softmax1d(y)
+print(y)
+print(p)
